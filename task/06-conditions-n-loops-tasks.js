45b4c26e7531912ce6dba8174cd5bd4072707f0e
Rumata <Rumatadeestor@gmail.com>
2016-05-01 21:41:20 +0300

    refactored

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -32,13 +32,14 @@
 function getFizzBuzz(num) {
     if (!(num % 15)) {
       return `FizzBuzz`;
-    } else if (!(num % 3)) {
+    } 
+    if (!(num % 3)) {
       return `Fizz`;
-    } else if (!(num % 5)) {
+    } 
+    if (!(num % 5)) {
       return `Buzz`;
-    } else {
+    } 
       return num;
-    }
 }
 
 
